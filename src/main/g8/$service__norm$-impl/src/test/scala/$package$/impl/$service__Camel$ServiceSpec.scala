e771c009e5ae960180af52855340c6a70d493e29
Elliot Marx <elliot@havenmoney.com>
2017-06-14 10:14:07 -0700

    Fix service spec import

--- src/main/g8/$service__norm$-impl/src/test/scala/$package$/impl/$service__Camel$ServiceSpec.scala
+++ src/main/g8/$service__norm$-impl/src/test/scala/$package$/impl/$service__Camel$ServiceSpec.scala
@@ -9,7 +9,7 @@ import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
 class $service;format="Camel"$ServiceSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
 
   lazy private val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra(true)) { ctx =>
-    new $service;format="Camel"$Application(ctx) with LocalServiceLocator
+    new app.$service;format="Camel"$Application(ctx) with LocalServiceLocator
   }
 
   val client: api.$service;format="Camel"$Service = server.serviceClient.implement[api.$service;format="Camel"$Service]
