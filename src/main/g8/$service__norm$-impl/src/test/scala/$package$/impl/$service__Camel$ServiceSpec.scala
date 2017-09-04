0780876aac3c309309a72d523c854ea7d2ac2828
Elliot Marx <elliot@havenmoney.com>
2017-09-04 12:11:24 -0700

    Fix spec template

--- src/main/g8/$service__norm$-impl/src/test/scala/$package$/impl/$service__Camel$ServiceSpec.scala
+++ src/main/g8/$service__norm$-impl/src/test/scala/$package$/impl/$service__Camel$ServiceSpec.scala
@@ -1,16 +1,22 @@
 package $package$.impl
 
+import com.lightbend.lagom.scaladsl.api.AdditionalConfiguration
 import com.lightbend.lagom.scaladsl.server.LocalServiceLocator
 import com.lightbend.lagom.scaladsl.testkit.ServiceTest
 import $package$.api
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
 import $package$.impl.app.$service;format="Camel"$Application
+import money.haven.utils.security.{ClientSecurity, JWTUtils, SimpleJwtAlgorithm}
 
 class $service;format="Camel"$ServiceSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
 
   lazy private val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra(true)) { ctx =>
-    new $service;format="Camel"$Application(ctx) with LocalServiceLocator
+    new $service;format="Camel"$Application(ctx) with LocalServiceLocator with SimpleJwtAlgorithm with TestTopicComponents {
+      override def additionalConfiguration: AdditionalConfiguration = {
+        super.additionalConfiguration ++ JdbcTestHelper.testServiceConfig("$service;format="Camel"$Spec")
+      }
+    }
   }
 
   val client: api.$service;format="Camel"$Service = server.serviceClient.implement[api.$service;format="Camel"$Service]
