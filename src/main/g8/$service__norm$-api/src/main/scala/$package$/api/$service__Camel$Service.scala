1447350cb40b4bdb3920f987c81f7ac109faf1f0
Elliot Marx <elliot@havenmoney.com>
2018-06-04 14:40:16 -0700

    Updates to template

--- src/main/g8/$service__norm$-api/src/main/scala/$package$/api/$service__Camel$Service.scala
+++ src/main/g8/$service__norm$-api/src/main/scala/$package$/api/$service__Camel$Service.scala
@@ -1,14 +1,15 @@
 package $package$.api
 
+import money.haven.utils.services.HavenInternalService
 import com.lightbend.lagom.scaladsl.api.{Descriptor, Service, ServiceCall}
 import play.api.libs.json.{Format, Json}
 
-trait $service;format="Camel"$Service extends Service {
+trait $service;format="Camel"$Service extends HavenInternalService {
   def apiCall: ServiceCall[String, akka.Done]
 
   override def descriptor: Descriptor = {
     import Service._
-    named("$service;format="camel"$").withCalls(
+    baseDescriptor("$service;format="camel"$").addCalls(
       pathCall("/api/", apiCall)
     )
   }
--- src/main/g8/$service__norm$-impl/src/test/scala/$package$/impl/$service__Camel$ServiceSpec.scala
+++ src/main/g8/$service__norm$-impl/src/test/scala/$package$/impl/$service__Camel$ServiceSpec.scala
@@ -1,39 +1,13 @@
-package $package$.impl
+package $package$.api
 
-import com.lightbend.lagom.scaladsl.api.AdditionalConfiguration
-import com.lightbend.lagom.scaladsl.server.LocalServiceLocator
-import com.lightbend.lagom.scaladsl.testkit.{ServiceTest, TestTopicComponents}
-import $package$.api
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, Matchers}
-import $package$.impl.app.$service;format="Camel"$Application
-import money.haven.utils.security.{ClientSecurity, JWTUtils, SimpleJwtAlgorithm}
-import money.haven.utils.JdbcTestHelper
+import money.haven.utils.specs.{HavenSpec, HavenSanitySpec}
 
-class $service;format="Camel"$ServiceSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll with ScalaFutures {
-
-  lazy private val server = ServiceTest.startServer(ServiceTest.defaultSetup.withCassandra(true)) { ctx =>
-    new $service;format="Camel"$Application(ctx) with LocalServiceLocator with SimpleJwtAlgorithm with TestTopicComponents {
-      override def additionalConfiguration: AdditionalConfiguration = {
-        super.additionalConfiguration ++ JdbcTestHelper.testServiceConfig("$service;format="Camel"$Spec")
-      }
-    }
-  }
-
-  val client: api.$service;format="Camel"$Service = server.serviceClient.implement[api.$service;format="Camel"$Service]
-
-  override def afterAll: Unit = server.stop()
-
-  override def beforeAll: Unit = server
-
-  "The $service;format="Camel"$ Service" should {
-    "" in {
-      for {
-        response <- client.apiCall.invoke("hello")
-      } yield {
-        1 should ===(1)
-      }
+class $service;format="Camel"$ServiceSanitySpec extends HavenSpec {
+  describe("$service;format="Camel"$Service") {
+    it("should describe all of its methods") {
+      ServiceSanitySpec.assertSanity[$service;format="Camel"$Service](
+        ServiceSanitySpec.getDescriptor[$service;format="Camel"$Service]
+      )
     }
   }
 }
-
